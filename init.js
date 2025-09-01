d2d2cced8bbd52f2606e42cdb116eadbe058b9e3
Jordan Lopez <jordanlopezemail@gmail.com>
2025-09-01 10:02:10 -0700

    first commit

---
+++ init.js
@@ -0,0 +1,5 @@
+//1: Compile Resume into a templatable format (Google Drive)
+//2: Scrape Postings using LinkedIn (Apify)
+//3: Compare job postings to bank of skills i have and filter job postings to ones i should actually apply for
+//4: Customize resume to jobs i should apply for
+//5: Find “Decision Makers” – those who i should email and beg for job (w/ resume attached)
